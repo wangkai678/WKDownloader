3e0f4bd1f23b1002880df14cbe320d8d7d2e1454
王凯 <2567851526@qq.com>
2017-09-09 22:12:27 +0800

    133

--- Example/Pods/Target Support Files/WKDownloader/WKDownloader-umbrella.h
+++ Example/Pods/Target Support Files/WKDownloader/WKDownloader-umbrella.h
@@ -1,17 +1,6 @@
-#ifdef __OBJC__
 #import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
-#endif
 
-#import "Dog.h"
-#import "Person.h"
+#import "WKDownLoader.h"
 
 FOUNDATION_EXPORT double WKDownloaderVersionNumber;
 FOUNDATION_EXPORT const unsigned char WKDownloaderVersionString[];
